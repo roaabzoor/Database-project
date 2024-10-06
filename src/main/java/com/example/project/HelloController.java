34586b68d91cd44a88a5336bb291089a050bc76c
roaabzoor <s12218040@stu.najah.edu>
2024-10-06 19:44:34 +0300

    first step

---
+++ src/main/java/com/example/project/HelloController.java
@@ -0,0 +1,37 @@
+package com.example.project;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
+
+    @FXML
+    private Hyperlink forget;
+
+    @FXML
+    private JFXButton login;
+
+    @FXML
+    private JFXTextField pass;
+
+    @FXML
+    private JFXTextField user;
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+
+}
