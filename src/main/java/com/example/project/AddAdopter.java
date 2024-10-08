c4d79d6cd5d7077c21f71597d3f9d7123f9137a7
roaabzoor <s12218040@stu.najah.edu>
2024-10-09 00:54:24 +0300

    roaaa

--- src/main/java/com/example/project/AddAdopter.java
+++ src/main/java/com/example/project/AddAdopter.java
@@ -1,4 +1,17 @@
 package com.example.project;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class AddAdopter {
+    @FXML
+    public AnchorPane adopter;
+    public void gotologin() throws IOException {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        adopter.getChildren().setAll(root);
+
+    }
 }
